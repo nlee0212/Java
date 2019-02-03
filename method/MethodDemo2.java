19a040984436c2e1618531af950a7b55868d3570
Na Yeon Lee <44570724+nlee0212@users.noreply.github.com>
2019-02-04 03:13:58 +0900

    Add files via upload

---
+++ method/MethodDemo2.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.method;
+
+public class MethodDemo2 {
+	public static void numbering(int init, int limit) {
+		int i=init;
+		while (i<limit) {
+			System.out.println(i);
+			i++;
+		}//Á¤ÀÇ
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		numbering(2,5);
+	}
+
+}
---
+++ object/CalculatorDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.object;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10+20);
+		System.out.println(20+40);
+	}
+
+}
